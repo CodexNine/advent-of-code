1a2e2c351cfb70ee2d2683c3a0dfc99fb9e7f2cb
CodexNine <dpvanderlaan@gmail.com>
2021-12-19 13:25:41 +0100

    test from terminal

--- day-1/src/main.go
+++ day-1/src/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/CodexNine/advent-of-code/day-1/src/pkg/readFile"
+	readFile "github.com/CodexNine/advent-of-code/day-1/src/pkg/readFile"
 )
 
 func main() {
